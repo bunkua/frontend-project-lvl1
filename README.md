614602b833bc6c2d160bacc507e6f14f6dc20010
Fedor <fs.golubkov@gmail.com>
2020-04-29 00:12:47 +0300

    Step 4

---
+++ README.md
@@ -0,0 +1,3 @@
+## Brain games
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/c70bd0f990846f3e0ed8/maintainability)](https://codeclimate.com/github/bunkua/frontend-project-lvl1/maintainability)
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const run = () => {
+  console.log('Welcome to Brain Games');
+  console.log('May I have your name?');
+  const name = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${name}`);
+};
+
+export default { run };
